3d89988d424aa4e850497c4ca67617b6f90df1d1
jnrubianoqantica <jrubiaob@usersad.everis.int>
2016-11-10 11:30:42 -0500

    AddRol0.1
    GUI para ingresar los roles

--- ejbModule/com/qantica/fedemarket/entidad/Rol.java
+++ ejbModule/com/qantica/fedemarket/entidad/Rol.java
@@ -9,9 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
+ * Entidad Rol
  * @author Juan Rubiano 
- * 08/09/2016
- * Q-antica Ltda.   
+ * 10/11/2016
  */
 
 @Entity
@@ -28,6 +28,9 @@ public class Rol implements Serializable{
 	@Column(name="nombre")
 	private String nombre;
 
+	@Column(name="estado")
+	private boolean estado;
+	
 	public int getId() {
 		return id;
 	}
@@ -43,7 +46,13 @@ public class Rol implements Serializable{
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}	
 	
 }
